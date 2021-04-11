585218406d8a25f6e897646afbfeb21c2b96c968
Leart992 <leartkamberi@hotmail.com>
2021-04-11 18:08:38 +0200

    SecondHomeworks

---
+++ FirstHomework/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace FirstHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sum of even numbers");
+
+
+            /*int[] evenNums = new int[6];
+            int sum = 0;
+
+            Console.WriteLine("Enter the first number");
+            evenNums[0] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number");
+            evenNums[1] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the third number");
+            evenNums[2] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the fourth number");
+            evenNums[3] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the fifth number");
+            evenNums[4] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the sixth number");
+            evenNums[5] = int.Parse(Console.ReadLine());
+
+
+
+            for (int i = 0; i < evenNums.Length; i++)
+            {
+                if (i % 2 == 0)
+                    sum += evenNums[i];
+                else
+                    Console.WriteLine("You number is not even");
+            }
+            */
+
+
+            int size;
+            int oddSum = 0, evenSum = 0;
+
+            Console.WriteLine("Enter the size of the array:");
+            size = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[size];
+            Console.WriteLine("Enter the array values:");
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    evenSum += arr[i];
+                }
+                else
+                {
+                    oddSum += arr[i];
+                }
+            }
+            Console.WriteLine("Even Sum: " + evenSum + " Odd Sum: " + oddSum);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SecondHomework/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace SecondHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Hello Student Group");
+
+
+            string[] studentsG1 = new string[] { "Jordan", "Nowitzki", "AbdulJabar", "Zubizareta", "Sampras" };
+
+
+            string[] studentsG2 = new string[] { "Maradona", "Pele", "Garbajosa", "Tupac", "Biggie" };
+
+
+
+            Console.WriteLine("Enter 1 or 2: ");
+            int input = int.Parse(Console.ReadLine());
+
+
+            /*
+            if (input == 1)
+            {
+
+
+                Console.WriteLine(" \n Second group");
+                foreach (string item in studentsG1)
+                
+
+                    Console.WriteLine(item);
+                        
+
+            }
+           
+
+            else if (input == 2)
+            {
+
+                Console.WriteLine(" \n Second group");
+                foreach (string item in studentsG2)
+                
+                    Console.WriteLine(item);
+              
+            }
+
+            else
+                Console.WriteLine("You entered a wrong number! ");
+
+            */
+
+            switch (input)
+            {
+                case 1:
+                    if (input == 1)
+                    {
+                        Console.WriteLine(" \n First group: ");
+                        foreach (string item in studentsG1)
+
+
+                            Console.WriteLine(item);
+                    }
+
+                    break;
+                case 2:
+                    if (input == 2)
+                    {
+                        Console.WriteLine(" \n Second group: ");
+                        foreach (string item in studentsG2)
+
+                            Console.WriteLine(item);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("You entered a wrong number!");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
